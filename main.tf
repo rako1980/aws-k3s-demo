97cc652a4dbbfb35149151934362081c46a052dd
Raj Adhikari <rajen.adhikari@gmail.com>
2019-05-05 21:43:39 +0000

    Added the ingress rules: port 80 and 443

--- main.tf
+++ main.tf
@@ -83,16 +83,31 @@ name = "k3s-ssh-sg"
 vpc_id = "${aws_vpc.signalpath.id}"
 
 ingress {
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+}
+ingress {
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+}
+ingress {
+  cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+}
 
-from_port = 22
-    to_port = 22
-    protocol = "tcp"
-  }
-
-  egress {
+egress {
    from_port = 0
    to_port = 0
    protocol = "-1"
